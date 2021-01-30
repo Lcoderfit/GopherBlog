36acc952bb3b4ccc616dfd47ecf08de79718cb50
Lcoderfit <1297611505@qq.com>
2021-01-30 22:19:08 +0800

    fix: 删除无关注释

--- main.go
+++ main.go
@@ -6,18 +6,6 @@ import (
 )
 
 func main() {
-	//user := model.User{
-	//	Username: "l00",
-	//	Password: "fajskdjf",
-	//	Role:     4,
-	//}
-	//msg, err := validator.Validate(&user)
-	//if err != nil {
-	//	utils.Logger.Error(err)
-	//	//utils.Logger.Error(msg)
-	//	fmt.Println("hk")
-	//}
-	//utils.Logger.Info(msg)
 	// 初始化数据库
 	model.InitDB()
 	// 初始化路由
