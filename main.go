2bf443f7bdc419c29c88c73f772056ff5f1ded45
Lcoderfit <1297611505@qq.com>
2021-02-09 23:56:40 +0800

    Fix：修改为REST风格的API

--- main.go
+++ main.go
@@ -2,16 +2,13 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	"GopherBlog/model"
+	"GopherBlog/routes"
 )
 
 func main() {
-	a, err := strconv.Atoi("1a0")
-	fmt.Println(a, err)
-
-	//// 初始化数据库
-	//model.InitDB()
-	//// 初始化路由
-	//routes.InitRouter()
+	// 初始化数据库
+	model.InitDB()
+	// 初始化路由
+	routes.InitRouter()
 }
